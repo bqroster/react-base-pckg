6781a3a2a79d571a9c2180f47463731d2bef84a3
Jose Burgos <fburgosq@gmail.com>
2019-06-18 22:19:57 -0400

    Add Enzyme for Testing components

---
+++ src/__tests__/basic.test.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import BasicText from '~/components/BasicText';
+import { shallow } from 'enzyme';
+
+describe('Basic Tests', () => {
+
+    test('Confirm that Jest runs successfully', () => {
+        const isValidTest = true;
+        expect(isValidTest).toBe(true);
+    });
+
+    test('Match <BasicText /> text content', () => {
+        const wrapper = shallow(<BasicText />);
+        expect(wrapper.text().toLowerCase()).toBe('react package basic component');
+    });
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 
-// ~components => set an alias on webpack.resolve
+// ~/components => set an alias on webpack.resolve
 import BasicText from '~/components/BasicText';
 
+// confirm that typescript is working HERE
 const contador: number = 3;
 
 class TextPackage extends React.Component {
